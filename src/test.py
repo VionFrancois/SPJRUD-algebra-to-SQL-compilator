9771b0c3fe00327e8c237b647aa90aae754c987e
VionFrancois <210809@umons.ac.be>
2022-11-20 17:50:22 +0100

    Ajout .gitignore

--- src/test.py
+++ src/test.py
@@ -7,8 +7,10 @@ attr1 = Attribute("a")
 attr2 = Attribute("Select")
 attr4 = Attribute("b")
 attr5 = Attribute("c")
-attr3 = Expression(attr4, attr2, attr5)
+attr6 = Expression(attr4, attr2, Attribute("d"))
+attr3 = Expression(attr4, attr2, attr6)
 expr = Expression(attr1,attr2,attr3)
+
 arbre.root = Node(expr)
 
 arbre.root.display()
