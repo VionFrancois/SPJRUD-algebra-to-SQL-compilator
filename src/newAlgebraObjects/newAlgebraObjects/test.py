d9aedd0d0d24c34e6686b4dc73ae95133dbb2bfc
Julien Ladeuze <89980155+EliotBD03@users.noreply.github.com>
2022-11-29 17:39:21 +0100

    Delete src/newAlgebraObjects/newAlgebraObjects directory

--- src/newAlgebraObjects/newAlgebraObjects/test.py
+++
@@ -1,7 +0,0 @@
-from request import Request
-from database import DataBase
-import sqlite3
-
-dt = DataBase("test.db")
-dt.execute(Request("project").make_request("*", "contacts"),"contacts")
-print(dt.display())
