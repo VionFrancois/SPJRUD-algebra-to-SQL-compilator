413f6f3db75de7bcb4e6dc22d655ccd8d748a93c
VionFrancois <210809@umons.ac.be>
2022-11-30 08:21:45 +0100

    Supression oldAlgebraObjects

--- src/AlgebraObjects/Attribute.py
+++
@@ -1,8 +0,0 @@
-class Attribute():
-    """
-    Représente tous les attributs de toutes les relations. 
-    Inutilisable pour les moments -> voir classe(s) enfant(es)
-    """
-
-    def __init__(self, name : str) -> None:
-        self.name = name
