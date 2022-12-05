01434a734f696a8378704f3135927794d632e2fb
EliotBD03 <julienladeuze@outlook.fr>
2022-12-05 09:57:23 +0100

    peut charger les anciennes requêtes

--- src/main.py
+++ src/main.py
@@ -6,11 +6,29 @@ from database import DataBase
 from request import Request
 import syntax
 
+import readline #utilisé pour remplir l'input avec les précédentes requêtes
+
+
+#https://stackoverflow.com/questions/2533120/show-default-value-for-editing-on-python-input-possible/2533142#2533142
+def rlinput(prompt, prefill='test'):
+   print(prompt, end = "")
+   readline.set_startup_hook(lambda: readline.insert_text(prefill))
+   try:
+      return input()  # or raw_input in Python 2
+   finally:
+      readline.set_startup_hook()
+
+
+def get_previous_request(file_name):
+    content = []
+    with open(file_name, "r") as file:
+        content = file.readlines()
+
+    return [x.strip() for x in content]
 
 
 if __name__ == "__main__":
     LOG = "request.log"
-    previous_request = []
 
 
     if len(argv) < 2:
@@ -18,19 +36,40 @@ if __name__ == "__main__":
         exit()
         
     database = DataBase(argv[1])
-    inp = None
     print("please enter a statement.")
-    with open(LOG, "a") as opened_file:
-        while True:
-            print("-> ", end = "")
-            inp = input()
 
-            if inp == "exit":
-                break
+    previous_request = get_previous_request(LOG)
+    pointer_file = len(previous_request)
 
-            if not syntax.syntax_is_correct(syntax.remove_space(inp)):
-                opened_file.write(inp+ "\n")
-                continue
+    new_request = []
+
+    while True:
+        print("-> ", end = "")
+        inp = input()
+
+        while inp == "&" and pointer_file > 0:
+            pointer_file -= 1
+            inp = rlinput("-> ", previous_request[pointer_file])
+        
+        if inp == "exit":
+            break
+
+        if pointer_file == 0 and inp == "&":
+            print("no more request")
+            continue
+
+        if not syntax.syntax_is_correct(syntax.remove_space(inp)):
+            continue
+        else:
             #TODO manque plus que l'execution de la requete apres la décomposition
+            if not inp in previous_request:
+                new_request.append(inp)        
+    
+    with open(LOG, "a") as file:
+        for request in new_request:
+            file.write(request + "\n")
 
-            
+
+"""
+Constaté un bug, on peut delete le prompt
+"""
