8887b0b5ac7909f9fd25ca338e03affe7865680f
VionFrancois <210809@umons.ac.be>
2022-12-06 13:54:55 +0100

    Pycache de ses morts

--- src/test.py
+++ src/test.py
@@ -1,24 +1,13 @@
-from entities import *
-from spjrud import *
-from SyntaxTree import *
+from request import Request
+from database import DataBase
 
-arbre = SyntaxTree()
-attr1 = Entity("a")
-attr2 = Entity("Select")
-attr4 = Entity("b")
-attr5 = Entity("c")
-attr6 = Expression(attr4, attr2, Entity("d"))
-attr3 = Expression(attr4, attr2, attr6)
-expr = Expression(attr1,attr2,attr3)
-firstReq = "Select(Att(Country),=,Cst(Mali),Re(CC))"
-secondReq = "Select(id,=,b,Join(b,c))"
+dt = DataBase("test.db")
+requete = Request("Rename").make_request("contacts", "contact_id", "id")
+print(requete)
+dt.execute(requete, "contacts")
+requet2 = Request("Project").make_request("contacts", "id")
+print(requet2)
+print(Request("Union").make_request(requet2, None, None, requete))
+dt.execute(Request("Difference").make_request(requete, None, None, requet2),"contacts")
 
-arbre.root = Node(expr)
-arbre = SyntaxTree.makeTree(secondReq)
-
-arbre.root.display()
-
-# print(arbre.root.left.attribute.name)
-# print(arbre.root.attribute.name)
-# print(arbre.root.right.attribute.name)
-arbre.display()
+print(dt.display())
